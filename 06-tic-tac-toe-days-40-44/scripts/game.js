9e8b2f3f5f01426314f64d86c4aafe7a94cdf30a
Ivana Stamenkovic <ivana.stamenkovic990@gmail.com>
2024-12-20 20:39:02 +0100

    Add JavaScript functions to check for a winner or draw

--- 06-tic-tac-toe-days-40-44/scripts/game.js
+++ 06-tic-tac-toe-days-40-44/scripts/game.js
@@ -54,7 +54,55 @@ function selectGameField(event) {
   }
 
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
-  console.log(gameData);
+  const winnerId = checkForGameOver();
 
+  currentRound++;
   switchPlayer();
+}
+
+function checkForGameOver() {
+  // checking the rows for equality
+  for (let i = 0; i < 3; i++)
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+
+  // checking the columns for equality
+  for (let i = 0; i < 3; i++)
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+
+  // checking the diagonal for equality (top left - bottom right)
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+
+  // checking the diagonal for equality (bottom left - top right)
+  if (
+    gameData[0][2] > 0 &&
+    gameData[0][2] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][0]
+  ) {
+    return gameData[0][2];
+  }
+
+  // checking for a draw
+  if (currentRound === 9) {
+    return -1;
+  }
+
+  return 0;
 }
