015ad6fe639d9eddc431351676e6723e56b61374
Ivana Stamenkovic <ivana.stamenkovic990@gmail.com>
2024-12-20 17:34:52 +0100

    Add JavaScript functions to manage game rounds and field selection

--- 06-tic-tac-toe-days-40-44/scripts/app.js
+++ 06-tic-tac-toe-days-40-44/scripts/app.js
@@ -1,4 +1,5 @@
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
   {
@@ -16,15 +17,22 @@ const backdropElement = document.getElementById("backdrop");
 const inputPlayerNameElement = document.getElementById("playername");
 const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors");
-const errorMsgNoNameOverlayElement = document.getElementById("error-msg-no-name");
-const errorMsgSameNameOverlayElement = document.getElementById("error-msg-same-name");
+const errorMsgNoNameOverlayElement =
+  document.getElementById("error-msg-no-name");
+const errorMsgSameNameOverlayElement = document.getElementById(
+  "error-msg-same-name"
+);
 const gameAreaElement = document.getElementById("active-game");
+const activePlayerNameElement = document.getElementById("active-player-name");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
 const startNewGameBtnElement = document.getElementById("start-game-btn");
-const closeErrorMsgBtnElements = document.getElementsByClassName("close-error-msg-btn");
+const closeErrorMsgBtnElements = document.getElementsByClassName(
+  "close-error-msg-btn"
+);
+const gameFieldElements = document.querySelectorAll("#game-board li");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
@@ -35,7 +43,13 @@ backdropElement.addEventListener("click", closePlayerConfig);
 formElement.addEventListener("submit", savePlayerConfig);
 
 startNewGameBtnElement.addEventListener("click", startNewGame);
-for (let btn of closeErrorMsgBtnElements) {
-  btn.addEventListener("click", closeErrorMsgOverlay);
+
+for (let closeErrorMsgBtnElement of closeErrorMsgBtnElements) {
+  closeErrorMsgBtnElement.addEventListener("click", closeErrorMsgOverlay);
 }
+
 backdropElement.addEventListener("click", closeErrorMsgOverlay);
+
+for (const gameFieldElement of gameFieldElements) {
+  gameFieldElement.addEventListener("click", selectGameField);
+}
--- 06-tic-tac-toe-days-40-44/scripts/game.js
+++ 06-tic-tac-toe-days-40-44/scripts/game.js
@@ -7,6 +7,7 @@ function startNewGame() {
     errorMsgSameNameOverlayElement.style.display = "block";
     backdropElement.style.display = "block";
   } else {
+    activePlayerNameElement.textContent = players[activePlayer].name;
     gameAreaElement.style.display = "block";
     gameAreaElement.scrollIntoView({ behavior: "smooth" });
   }
@@ -16,4 +17,27 @@ function closeErrorMsgOverlay() {
   errorMsgNoNameOverlayElement.style.display = "none";
   errorMsgSameNameOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+}
+
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+
+  activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+  const selectedField = event.target;
+  selectedField.textContent = players[activePlayer].symbol;
+
+  if (activePlayer === 0) {
+    selectedField.classList.add("disabled-x");
+  } else if (activePlayer === 1) {
+    selectedField.classList.add("disabled-o");
+  }
+
+  switchPlayer();
 }
